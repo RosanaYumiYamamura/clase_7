4a878a1e3100b1f85ec21b824552a550995ab867
Rosana Yumi Yamamura <127357201+RosanaYumiYamamura@users.noreply.github.com>
2023-04-01 15:26:16 -0300

    Update multiplicar.ts

--- multiplicar.ts
+++ multiplicar.ts
@@ -1,15 +1,13 @@
 //Algoritmo que muestre la tabla de multiplicación del "x"# y hasta que # ingresado por el usuario//
-//Ingrese el número: 9------Ingrese hasta qué número: 4///
+//Ingrese el número que quiere multiplicar: 9------Ingrese hasta qué número quiere multiplicar: 4///
+//aplicar en terminal: npm i --save-dev @types/jquery
+
 import * as rls from "readline-sync";
-let no1 : number = rls.questionInt("Ingrese el número: ");
-let no2 : number = rls.questionInt("Ingrese hasta qué número: ");
-let result : number = 0;
-if (no1 < 0) {
-result = no1*no2;
-} else {
-result = no1*no2;
+
+let no1 : number = rls.questionInt("Ingrese el número que quiere multiplicar: ");
+let no2 : number = rls.questionInt("Ingrese hasta qué número quiere multiplicar: ");
+
+for (let i:number = 1; i <= no2; i++){
+    let result: number = no1*i;
+    console.log(`${no1} x ${i} = ${result}`);
 }
-console.log("9x1=1");
-console.log("9x2=18");
-console.log("9x3=27");
-console.log("9x4=" + result);
